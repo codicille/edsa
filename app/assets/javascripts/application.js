3653691d50a63822c025c4ec88fb94f643c551b7
Etienne Lemay <etiennelemay21@gmail.com>
2013-05-07 14:12:57 -0400

    Replace history state on scroll & resize

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,75 +1,62 @@
 //= require_tree ./ext
 //= require_self
 
-// Cached elements
-var $body, $window, defaultTitle;
+// Global variables & cached elements
+var $body, $window, defaultTitle, currentState;
 
 $body = $('body');
 $window = $(window);
+
 defaultTitle = document.title;
+currentState = null;
+advancedMenusOpened = false;
+currentScrollTop = 0;
 
 // Advanced menus
 showAdvancedMenus = function() {
+  if (advancedMenusOpened) { return }
+  advancedMenusOpened = true;
+  currentScrollTop = getScrollTop();
+
   $body.addClass('show-advanced-menus');
-  initScroll();
 }
 
 hideAdvancedMenus = function() {
+  if (!advancedMenusOpened) { return }
+  advancedMenusOpened = false;
+
   $body.removeClass('show-advanced-menus');
-  stopScroll();
 }
 
 getScrollTop = function() {
   return window.pageYOffset || (typeof $window.scrollTop === "function" ? $window.scrollTop() : 0);
 }
 
-stopScroll = function(e) { $window.off('scroll') }
-initScroll = function(e) {
-  var initialScrollTop = getScrollTop();
-
-  $window.on('scroll', function(e) {
-    var scrollTop, difference;
-
-    scrollTop = getScrollTop();
-    difference = Math.abs(scrollTop - initialScrollTop);
-
-    if (difference > 100) { hideAdvancedMenus() }
-  })
-}
-
 // History replace state
-getAnchorTypeAndNumberMatches = function(url) {
-  return url.match(/(paragraph|chapter)(-|\/)([0-9]+)/);
+getAnchorTypeAndNumberMatches = function(string) {
+  var matches = string.match(/(paragraph|chapter)(-|\/)([0-9]+)/);
+  if (matches == null) { return null }
+
+  return { type: matches[1], number: matches[3] }
 }
 
 gotoAnchor = function(anchorType, anchorNumber) {
   var id, $elem, offset;
 
-  id = '#' + anchorType + '-' + anchorNumber
-  $elem = $(id)
-  offset = $elem.offset().top
+  id = '#' + anchorType + '-' + anchorNumber;
+  $elem = $(id);
+  offset = $elem.offset().top;
 
-  $window.scrollTop(offset)
+  $window.scrollTop(offset);
   document.title = defaultTitle + ' | ' + anchorType.capitalize() + ' ' + anchorNumber;
 }
 
-$('.paragraph-count').on('click', function(e) {
-  var matches, anchorType, anchorNumber, state, title, url;
-  e.preventDefault();
+gotoAnchorFromMatches = function(matches) {
+  if (matches == null) { return }
+  gotoAnchor(matches.type, matches.number);
+}
 
-  matches = getAnchorTypeAndNumberMatches(this.getAttribute('href'));
-  anchorType = matches[1];
-  anchorNumber = matches[3];
-
-  state = {};
-  title = defaultTitle + ' - ' + anchorType.capitalize() + ' ' + anchorNumber;
-  url = '/' + anchorType + '/' + anchorNumber;
-
-  history.replaceState(state, title, url)
-  gotoAnchor(anchorType, anchorNumber)
-})
-
-initCurrentAnchor = function() {
+gotoCurrentAnchor = function() {
   var currentHref, matches;
 
   currentHref = window.location.href;
@@ -77,10 +64,97 @@ initCurrentAnchor = function() {
 
   if (matches == null) { return }
 
-  anchorType = matches[1];
-  anchorNumber = matches[3];
-
-  gotoAnchor(anchorType, anchorNumber);
+  gotoAnchorFromMatches(matches);
 }
 
-initCurrentAnchor()
+replaceState = function(anchorType, anchorNumber, scrollToAnchor) {
+  if (currentState && currentState.type == anchorType && currentState.number == anchorNumber) { return }
+
+  var state, title, url;
+  if (scrollToAnchor == null) { scrollToAnchor = false }
+
+  state = { type: anchorType, number: anchorNumber };
+  title = defaultTitle + ' | ' + anchorType.capitalize() + ' ' + anchorNumber;
+  url = '/' + anchorType + '/' + anchorNumber;
+
+  history.replaceState(state, title, url);
+  currentState = state;
+
+  if (scrollToAnchor) {
+    gotoAnchor(anchorType, anchorNumber);
+  } else {
+    document.title = title;
+  }
+}
+
+replaceStateFromMatches = function(matches, scrollToAnchor) {
+  if (matches == null) { return }
+  replaceState(matches.type, matches.number, scrollToAnchor);
+}
+
+clearState = function() {
+  if (currentState == null) { return }
+  currentState = null;
+
+  var state, title, url;
+
+  state = {};
+  title = defaultTitle;
+  url = '/';
+
+  history.replaceState(state, title, url);
+  document.title = title;
+}
+
+isInTheFold = function(elem) {
+  var rect = elem.getBoundingClientRect();
+  if (rect.top <= 0 && rect.bottom > 0) { return true }
+  return false;
+}
+
+onParagraphCountClick = function(e) {
+  e.preventDefault();
+
+  var matches = getAnchorTypeAndNumberMatches(this.getAttribute('href'));
+  replaceStateFromMatches(matches, true);
+}
+
+onWindowScroll = function(e) {
+  var scrollTop, scrollDifference, currentAnchor;
+
+  scrollTop = getScrollTop();
+  currentAnchor = null;
+
+  // Close advanced menus
+  if (advancedMenusOpened) {
+    scrollDifference = Math.abs(scrollTop - currentScrollTop);
+    if (scrollDifference > 100) { hideAdvancedMenus() }
+  }
+
+  // Change history state to current chapter & paragraph
+  $('.chapter').each(function(i, chapter) {
+    if (!isInTheFold(chapter)) { return true }
+    currentAnchor = chapter;
+
+    $(chapter).children('.paragraph').each(function(ii, paragraph) {
+      if (!isInTheFold(paragraph)) { return true }
+      currentAnchor = paragraph;
+    });
+  });
+
+  if (currentAnchor == null) {
+    clearState();
+    return;
+  }
+
+  matches = getAnchorTypeAndNumberMatches(currentAnchor.id);
+  replaceStateFromMatches(matches, false);
+}
+
+// Events
+$('.paragraph-count').on('click', onParagraphCountClick);
+$window.on('scroll', onWindowScroll);
+$window.on('resize', onWindowScroll);
+
+// Onload
+gotoCurrentAnchor();
