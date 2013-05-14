78a3bd519e746c516d78c87b9f8fa392ab0b68af
Etienne Lemay <etiennelemay21@gmail.com>
2013-05-14 15:17:27 -0400

    Font-size settings

--- app/assets/javascripts/app/readability_settings.js
+++ app/assets/javascripts/app/readability_settings.js
@@ -3,22 +3,34 @@ ReadabilitySettings.prototype = (function() { var pro = {};
 
   // Global variables
   var options = {
-    submenuOpened: false
+    submenuOpened: false,
+    fontSize: {
+      default: 0,
+      current: 0
+    }
   }
 
   // jQuery cached elements
   var elements = {
-    container: $('.readability-settings-container')
+    container: $('.readability-settings-container'),
+    main: $('[role="main"]')
   }
 
   // Public scope --------------------------------------------------------------
   pro.initialize = function(args) {
+    var mainComputedStyle = window.getComputedStyle(elements.main[0]);
+
+    setFontSizeOptions(mainComputedStyle);
     initContrastSlider();
   }
 
   pro.toggleSubmenu = function() {
     options.submenuOpened ? closeSubmenu() : openSubmenu();
   }
+
+  pro.largerFontSize  = function() { changeFontSize(1) }
+  pro.smallerFontSize = function() { changeFontSize(-1) }
+  pro.normalFontSize  = function() { setFontSize(options.fontSize.default) }
 
   // Private scope -------------------------------------------------------------
   var openSubmenu = function() {
@@ -45,6 +57,26 @@ ReadabilitySettings.prototype = (function() { var pro = {};
         // console.log(ui.value);
       }
     })
+  }
+
+  // Font-size Management
+  var setFontSizeOptions = function(mainComputedStyle) {
+    var mainFontSize = parseInt(mainComputedStyle.getPropertyValue('font-size'));
+
+    options.fontSize.default = mainFontSize;
+    options.fontSize.current = mainFontSize;
+  }
+
+  var changeFontSize = function(increment) {
+    var newFontSize = options.fontSize.current + increment;
+    setFontSize(newFontSize);
+  }
+
+  var setFontSize = function(fontSize) {
+    if (options.fontSize.current == fontSize) { return }
+    options.fontSize.current = fontSize;
+
+    elements.main.css('font-size', fontSize + 'px');
   }
 
 return pro })();
