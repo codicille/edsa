72a263084cb0ac99ff8b3b7bed21ce202367933e
Etienne Lemay <etiennelemay21@gmail.com>
2013-05-22 14:54:56 -0400

    Rename “App” => “UA” in head_script.js

--- app/assets/javascripts/head_script.js
+++ app/assets/javascripts/head_script.js
@@ -2,35 +2,35 @@ var elem, html, prefix, prefixes, _i, _j, _len, _len1, _ref;
 html = document.documentElement;
 
 // Global Namespace
-window.App = {};
+window.UA = {};
 
 // Touch
-App.IS_TOUCH_DEVICE = window.ontouchend !== void 0;
+UA.IS_TOUCH_DEVICE = window.ontouchend !== void 0;
 
 // Transition
-App.HAS_TRANSITIONS = false;
+UA.HAS_TRANSITIONS = false;
 prefixes = ['webkit', 'Moz', 'Ms', 'O'];
 for (_i = 0, _len = prefixes.length; _i < _len; _i++) {
   prefix = prefixes[_i];
   if (html.style["" + prefix + "Transition"] !== void 0) {
     html.className += " " + (prefix.toLowerCase());
-    App.HAS_TRANSITIONS = true;
-    App.BROWSER = prefix.toLowerCase();
+    UA.HAS_TRANSITIONS = true;
+    UA.BROWSER = prefix.toLowerCase();
   }
 }
 
 // SVG
-App.HAS_SVG = !!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect;
+UA.HAS_SVG = !!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect;
 
 // Retina
-App.IS_RETINA = ('devicePixelRatio' in window && devicePixelRatio > 1) || ('matchMedia' in window && matchMedia("(min-resolution:144dpi)").matches);
+UA.IS_RETINA = ('devicePixelRatio' in window && devicePixelRatio > 1) || ('matchMedia' in window && matchMedia("(min-resolution:144dpi)").matches);
 
 // HTML Classes
 html.className = html.className.replace('no-js', 'js');
-html.className += App.IS_TOUCH_DEVICE ? ' touch' : ' no-touch';
-html.className += App.HAS_TRANSITIONS ? ' transitions' : ' no-transitions';
-html.className += App.HAS_SVG ? ' svg' : ' no-svg';
-html.className += App.IS_RETINA ? ' retina' : ' no-retina';
+html.className += UA.IS_TOUCH_DEVICE ? ' touch' : ' no-touch';
+html.className += UA.HAS_TRANSITIONS ? ' transitions' : ' no-transitions';
+html.className += UA.HAS_SVG ? ' svg' : ' no-svg';
+html.className += UA.IS_RETINA ? ' retina' : ' no-retina';
 
 if (/\bipad\b/i.test(navigator.userAgent)) { html.className += ' ios ipad'; }
 if (/\biphone\b/i.test(navigator.userAgent)) { html.className += ' ios iphone'; }
