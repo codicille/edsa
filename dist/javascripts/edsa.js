5680484cc47a3f2ec4c9329ed10fcd494969e339
Louis-Philippe Cloutier <louisphilippe.cloutier@hooktstudios.com>
2013-11-21 14:39:32 -0500

    Add missing onTap plugins into js requires

--- dist/javascripts/edsa.js
+++ dist/javascripts/edsa.js
@@ -27,6 +27,32 @@ String.prototype.capitalize = function() {
 }
 
 
+$.fn.onTap = function(callback) {
+  if (window.ontouchend !== void 0) {
+    // Init touchMoveCount
+    this.each(function() {
+      this._touchMoveCount = 0
+    });
+
+    // Count touch moves
+    this.on('touchmove', function() {
+      this._touchMoveCount++;
+    });
+
+    // callback() if touchMoveCount is < 3
+    this.on('touchend', function(e) {
+      if (this._touchMoveCount < 3) { callback.call(this, e) }
+      this._touchMoveCount = 0;
+    });
+
+    // Cancel click event
+    this.on('click', function() {
+      return false;
+    });
+  }
+}
+
+
 var ButtonsGroup = (function() {
   function ButtonsGroup($elem, $defaultSelectedButton) {
     // jQuery cached elements
--- src/javascripts/edsa.js
+++ src/javascripts/edsa.js
@@ -1,5 +1,6 @@
 require('ext/bind');
 require('ext/string');
+require('plugins/on_tap.js');
 require('ui/buttons_group');
 require('head_script');
 require('app/app');
