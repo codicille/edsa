4e9bd3d14e440b96b2e7c1c52442faae4f889b3b
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2013-11-26 15:27:56 -0500

    Add grunt task for dist vs development application

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,53 +1,66 @@
 /*jshint camelcase: false */
 /*global module:false */
 module.exports = function(grunt) {
+  var neuterOptions = {
+    template: '{%= src %}',
+    includeSourceURL: true,
+    basePath: 'src/javascripts/'
+  };
+
   grunt.initConfig({
+    neuter: {
+      application: {
+        options: neuterOptions,
+        src: 'src/javascripts/edsa.js',
+        dest: 'src/dev/javascripts/edsa.js'
+      },
+      dist: {
+        options: neuterOptions,
+        src: 'src/javascripts/edsa.js',
+        dest: 'dist/javascripts/edsa.js'
+      }
+    },
+    sass: {
+      application: { files: { 'src/dev/stylesheets/edsa.css': 'src/stylesheets/edsa.scss' } },
+      dist: { files: { 'dist/stylesheets/edsa.css': 'src/stylesheets/edsa.scss' } }
+    },
+    // Source only development tasks
     connect: {
-      dev: {
+      application: {
         options: {
-          base: 'dist',
+          base: 'src/dev',
           port: 9002,
           hostname: '*'
         }
       }
     },
-    neuter: {
-      options: {
-        template: '{%= src %}',
-        includeSourceURL: true,
-        basePath: 'src/javascripts/'
-      },
-      'dist/javascripts/edsa.js': 'src/javascripts/edsa.js'
-    },
     watch: {
-      application_code: {
+      neuter: {
         files: ['src/javascripts/**/*.js'],
-        tasks: ['neuter', 'uglify']
+        tasks: ['neuter:application']
       },
       sass: {
         files: ['src/stylesheets/*.scss'],
-        tasks: ['sass']
+        tasks: ['sass:application']
       }
     },
+    // Dist only tasks
     uglify: {
       'dist/javascripts/edsa.min.js': 'dist/javascripts/edsa.js'
     },
-    sass: {
-      dist: {
-        files: {
-          'dist/stylesheets/edsa.css': 'src/stylesheets/edsa.scss',
-        }
-      }
-    }
   });
 
   grunt.registerTask('default', [
     'connect',
-    'neuter',
-    'sass',
-    'uglify',
+    'neuter:application',
+    'sass:application',
     'watch',
   ]);
 
+  grunt.registerTask('dist', [
+    'neuter:dist',
+    'sass:dist',
+    'uglify'
+  ])
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 };
